7e6f491f09a6d4e302f9bd32e921f4dfca668982
Alexandre Santos <ajvsms@gmail.com>
2015-05-15 11:39:11 +0100

    Update docker-entrypoint.sh

--- TSAM-Sync/docker-entrypoint.sh
+++ TSAM-Sync/docker-entrypoint.sh
@@ -6,4 +6,6 @@ echo webservice.user=$WEBSERVICE_USER >> $TSAM_HOME/conf/settings.properties
 echo webservice.password=$WEBSERVICE_PASSWORD >> $TSAM_HOME/conf/settings.properties
 echo sync.countrycode=$SYNC_COUNTRYCODE >> $TSAM_HOME/conf/settings.properties
 
+mysql -htsamdb -P3306 -uroot -p$MYSQL_ENV_MYSQL_ROOT_PASSWORD < create_tsam_database.sql
+
 $TSAM_HOME/sync.sh
