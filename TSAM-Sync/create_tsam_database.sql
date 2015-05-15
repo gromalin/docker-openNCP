c597c845339fc6fed103e8ef185d519d598e4246
Alexandre Santos <ajvsms@gmail.com>
2015-05-15 11:37:55 +0100

    Update create_tsam_database.sql

--- TSAM-Sync/create_tsam_database.sql
+++ TSAM-Sync/create_tsam_database.sql
@@ -1,10 +1,10 @@
-CREATE DATABASE epsos_properties;
+CREATE DATABASE IF NOT EXISTS epsos_properties CHARACTER SET utf8;
 GRANT ALL ON epsos_properties.* TO epsos IDENTIFIED BY 'epsos.Pass';
 
-CREATE DATABASE ltrdb;
+CREATE DATABASE IF NOT EXISTS ltrdb CHARACTER SET utf8;
 GRANT ALL ON ltrdb.* TO tsam IDENTIFIED BY 'tsam.Pass';
 
-CREATE TABLE `epsos_properties`.`property` (
+CREATE TABLE IF NOT EXISTS `epsos_properties`.`property` (
   `name` varchar(255) NOT NULL,
   `value` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`name`)
